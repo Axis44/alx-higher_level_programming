639d65e02a366c3b31f1293ed990e5cf3f35f3b1
Axis44 <kunenexolani34@gmail.com>
2023-08-08 07:00:34 -0700

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for t in str:
-        if ord("a") <= ord(t) <= ord("z"):
-            t = chr(ord(t) - 32)
-        print("{:s}".format(t), end="")
-    print()
+def print_last_digit(number):
+    if number >= 0:
+        lst_digit = number % 10
+    else:
+        lst_digit = number % -10
+        lst_digit *= -1
+
+    print("{:d}".format(lst_digit), end='')
+    return (lst_digit)
