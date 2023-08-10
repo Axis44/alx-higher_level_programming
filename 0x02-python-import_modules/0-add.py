7cfb06cb76c420dd5115245be27c745ba103b3d9
Axis44 <kunenexolani34@gmail.com>
2023-08-10 10:14:03 -0700

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-    """print addition of 1 and 2 function"""
+if __name__ == "__main__": #addition of a and b
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
