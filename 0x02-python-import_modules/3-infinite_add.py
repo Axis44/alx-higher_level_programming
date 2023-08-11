46d3c3bf6f79a57eec7be682bec39ecc11ed3347
Axis44 <kunenexolani34@gmail.com>
2023-08-11 01:40:06 -0700

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    "" adds all arguments""
+    #adds all arguments
     import sys
-    result = 0
-     for t in range(len(sys.argv) - 1):
-        result += int(sys.argv[t + 1])
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
