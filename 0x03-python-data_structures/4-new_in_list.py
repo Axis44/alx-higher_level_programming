7bcfa674a34d45881dec1c9aae6cf45995e5df93
Axis44 <kunenexolani34@gmail.com>
2023-08-14 04:24:02 -0700

    replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# replaces an element in list specific position without modifying original list
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
