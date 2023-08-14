a2bb215d60c0ffea830df0e9fea78cd47f797266
Axis44 <kunenexolani34@gmail.com>
2023-08-14 00:08:12 -0700

    replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# replaces element of list at specific line
+
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
