e7cae6c67e71a7b18a1c8ecd4fe8c0b87a62c500
Axis44 <kunenexolani34@gmail.com>
2023-08-15 00:49:06 -0700

    print name

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    #Print all hidden_4 module names
+    # Print all hidden_4 module names
     import hidden_4
 
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
-            print(name
+            print(name)
