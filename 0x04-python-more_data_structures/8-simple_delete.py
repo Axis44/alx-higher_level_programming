1fbb7db906ca90b63fc35c8a272d5e9de21f2c42
Axis44 <kunenexolani34@gmail.com>
2023-08-16 10:00:25 -0700

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(my_dictionary, key=""):
     # delete dictionary key
-    if a_dictionary.get(key) is not None:
-        del a_dictionary[key]
-    return (a_dictionary)
+    new_dict = {}
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
