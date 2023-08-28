3a45f2d7ebc3a31be41d78fbab4c5c022f664c11
Axis44 <kunenexolani34@gmail.com>
2023-08-28 05:39:36 -0700

    elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+# print range
 def safe_print_list(my_list=[], x=0):
     num = 0
     for n in range(x):
         try:
-            print(my_list[n], end="")
+            print("{}".format(my_list[n]), end="")
             num += 1
         except IndexError:
             break
     print("")
-    Return
+    Return(num)
