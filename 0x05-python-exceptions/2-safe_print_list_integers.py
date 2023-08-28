caf0fa95122a70b6c0af43ccab4a95592b397845
Axis44 <kunenexolani34@gmail.com>
2023-08-28 07:15:58 -0700

    count int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,12 @@
 
 # print first integers in list
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for n in range(x):
+    ret = 0
+    for n in range(0, x):
         try:
             print("{:d}".format(my_list[n]), end="")
-            count += 1
+            ret += 1
         except (ValueError, TypeError):
-            pass
-    print()
-    Return
+            continue
+    print("")
+    return (ret)
