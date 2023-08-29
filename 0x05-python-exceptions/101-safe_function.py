1618dd96aeaeca1e6486d4d68f57fe35ea95f5c3
Axis44 <kunenexolani34@gmail.com>
2023-08-28 22:44:38 -0700

    safe

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,11 +2,13 @@
 
 import sys
 # execute function
+
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    
-    except:
+
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
